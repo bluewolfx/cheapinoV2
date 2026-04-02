c5e6e53f77ec3a35b9f2b7028f66fab23f3d5b94
bluewolfx <ricardobanon@gmail.com>
2026-04-02 04:28:26 +0100

    fix issue with unlock native keys for physical ones

--- keyboards/cheapino/keymaps/vial/config.h
+++ keyboards/cheapino/keymaps/vial/config.h
@@ -4,8 +4,8 @@
 #define VIAL_KEYBOARD_UID {0x79, 0xED, 0x40, 0xBB, 0x09, 0x8B, 0xC1, 0x9E}
 
 /* Unlock combo: both outer thumb keys */
-#define VIAL_UNLOCK_COMBO_ROWS {3, 3}
-#define VIAL_UNLOCK_COMBO_COLS {2, 5}
+#define VIAL_UNLOCK_COMBO_ROWS {6, 2}
+#define VIAL_UNLOCK_COMBO_COLS {11, 5}
 
 #define QUICK_TAP_TERM 120
 #define NUM_ENCODERS 1
